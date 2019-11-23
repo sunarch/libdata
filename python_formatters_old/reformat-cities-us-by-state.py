f1d9d4e477d2ba8395703c92db7f31d130f974c5
András Németh <sunarchx@gmail.com>
2019-11-23 13:47:27 +0100

    Remove old license notices

--- python_formatters_old/reformat-cities-us-by-state.py
+++ python_formatters_old/reformat-cities-us-by-state.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python3
 
-#
-# < libdata > - < Library of data files and formatters >
-#
-# Written in 2017 by András Németh <sunarchx@gmail.com>
-#
-# To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
-#
-# You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-#
-
 infile = open('cities-us-by-state.txt', 'r')
 #outfile =  open('cities-us-by-state.py', 'w')
 '''
